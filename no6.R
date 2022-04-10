19d11eeec8c42d04991e2561e4548e57ba940d53
AiffahKiysa <kiysa.205025@mhs.its.ac.id>
2022-04-10 18:14:14 +0700

    commit

--- no6.R
+++ no6.R
@@ -3,13 +3,10 @@ n <- 100
 mean <- 50
 sd <- 8
 y <- rnorm(n, mean, sd)
-hist(y, probability = TRUE)
 
-set.seed(1)
-n <- 100
-mean <- 50
-sd <- 8
-rnorm(n, mean, sd)
+z <- function(input){
+  return ((1 / (sd * sqrt(2*22/7))) * exp(1) ^ (-0.5 * ((input - rata) / sd) ^ 2))
+}
 
-z <- (n-mean)/sd
-pnorm(z)
+z(x1 <- floor(mean(x)))
+z(x1 <- ceiling(mean(x)))
